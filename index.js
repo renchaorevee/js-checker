5575d28c3a00fbaae9ee8d918388553df3b71947
Chao Ren <chaoren@us.ibm.com>
2015-08-09 21:22:52 -0700

    DRY type checking code

--- index.js
+++ index.js
@@ -4,30 +4,16 @@
  * Checking Types:
  * Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
  */
-exports.isTypeUndefined = function(input) {
-    return (typeof input === 'undefined');
-}
-
-exports.isTypeObject = function(input) {
-    return (typeof input === 'object');
-}
-
-exports.isTypeBoolean = function(input) {
-    return (typeof input === 'boolean');
-}
-
-exports.isTypeNumber = function(input) {
-    return (typeof input === 'number');
-}
-
-exports.isTypeString = function(input) {
-    return (typeof input === 'string');
-}
-
-exports.isTypeSymbol = function(input) {
-    return (typeof input === 'symbol');
-}
-
-exports.isTypeFunction = function(input) {
-    return (typeof input === 'function');
-}
+[
+    'Undefined',
+    'Object',
+    'Boolean',
+    'Number',
+    'String',
+    'Symbol',
+    'Function'
+].forEach(function(type) {
+    exports['isType' + type] = function(input) {
+        return (typeof input === type.toLowerCase());
+    }
+});
